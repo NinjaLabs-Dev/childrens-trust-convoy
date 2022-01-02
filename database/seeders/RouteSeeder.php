0a2d708e40e8687a6e7fda14c179132fca198ec5
NinjaLabs-Dev <fraser@ninjalabs.dev>
2022-01-02 00:43:07 +0000

    Site

--- database/seeders/RouteSeeder.php
+++ database/seeders/RouteSeeder.php
@@ -20,7 +20,7 @@ class RouteSeeder extends Seeder
                 'start' => 'Stavanger',
                 'start_time' => 1644670800,
                 'end' => 'Stavanger',
-                'image' => 1,
+                'image' => '1',
                 'truckfest' => true
             ],
             [
@@ -28,63 +28,63 @@ class RouteSeeder extends Seeder
                 'start' => 'Stavanger',
                 'start_time' => 1644674400,
                 'end' => 'Karlskrona',
-                'image' => 1
+                'image' => 'https://i.imgur.com/3mFbASX.jpg'
             ],
             [
                 'number' => 2,
                 'start' => 'Gdansk',
                 'start_time' => 1644683400,
                 'end' => 'Daugavpils',
-                'image' => 1
+                'image' => 'https://i.imgur.com/Ew1UhpS.png'
             ],
             [
                 'number' => 3,
                 'start' => 'Daugavpils',
                 'start_time' => 1644692400,
                 'end' => 'Lublin',
-                'image' => 1
+                'image' => 'https://i.imgur.com/8TVe1DA.jpg'
             ],
             [
                 'number' => 4,
                 'start' => 'Lublin',
                 'start_time' => 1644703200,
                 'end' => 'Lasi',
-                'image' => 1
+                'image' => 'https://i.imgur.com/OHYqwoj.jpg'
             ],
             [
                 'number' => 5,
                 'start' => 'Lasi',
                 'start_time' => 1644712200,
                 'end' => 'Pecs',
-                'image' => 1
+                'image' => 'https://i.imgur.com/SUMPEK6.jpg'
             ],
             [
                 'number' => 6,
                 'start' => 'Pecs',
                 'start_time' => 1644721200,
                 'end' => 'Innsbruck',
-                'image' => 1
+                'image' => 'https://i.imgur.com/WsaM7Jf.jpg'
             ],
             [
                 'number' => 7,
                 'start' => 'Innsbruck',
                 'start_time' => 1644728400,
                 'end' => 'Livorno',
-                'image' => 1
+                'image' => 'https://i.imgur.com/6dFqntY.jpg'
             ],
             [
                 'number' => 8,
                 'start' => 'Livorno',
                 'start_time' => 1644741000,
                 'end' => 'Bayonne',
-                'image' => 1
+                'image' => 'https://i.imgur.com/FrMqNaI.jpg'
             ],
             [
                 'number' => 9,
                 'start' => 'Bavonne',
                 'start_time' => 1644750000,
                 'end' => 'Cordoba',
-                'image' => 1
+                'image' => 'https://i.imgur.com/KNlUrlt.png'
             ],
         ];
 
