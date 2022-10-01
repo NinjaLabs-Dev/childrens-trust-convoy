0eed00d14c2a8054bba4daf881f0a9a9a2f2d474
Ninja <fraser@ninjalabs.co.uk>
2022-10-01 21:50:21 +0100

    Update AboutUs.vue

--- resources/js/components/AboutUs.vue
+++ resources/js/components/AboutUs.vue
@@ -6,7 +6,7 @@
             <div class="col-xl-6 p-4 content p-2">
                 <h2 class="mb-4">About Us</h2>
                 <div>
-                    <p>The Children's Trust is a charity that helps children with brain injuries, and we value their work. That's why we're driving for 24 hours in convoy to raise funds and awareness for this amazing charity. On the 12th and 13th February we'll be driving across the entirety of Europe in ETS2, to have an awesome day and night, and raise as much money as we can.</p>
+                    <p>The Children's Trust is a charity that helps children with brain injuries, and we value their work. That's why we'll be driving across Europe for 12 hours to raise as much awareness and funds as we can for this awesome charity. The event is scheduled to take place on the 11th February, and we'd love to see as many of you there as possible!</p>
                 </div>
             </div>
         </div>
